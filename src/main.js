81688793ab49f40bd7110810a1316d4f278af57d
Shiqan <ferron.saan@live.nl>
2018-07-05 19:23:55 +0200

    graphcms init

---
+++ src/main.js
@@ -0,0 +1,29 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+import { ApolloClient } from 'apollo-client'
+import { HttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import VueApollo from 'vue-apollo'
+import router from './router.js'
+
+const GRAPHCMS_API = 'YOUR_GRAPHCMS_API'
+​
+const apolloClient = new ApolloClient({
+  link: new HttpLink({ uri: GRAPHCMS_API }),
+  cache: new InMemoryCache()
+})
+
+Vue.use(VueApollo)
+
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient
+})
+​
+new Vue({
+  el: '#app',
+  provide: apolloProvider.provide(),
+  router,
+  template: '<App/>',
+  components: { App }
+})
